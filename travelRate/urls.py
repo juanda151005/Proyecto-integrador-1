27bc698685bf378cd3a9b977128f0129e3d1b667
Bayron Mena <bayronmenar@gmail.com>
2024-08-24 21:17:45 -0500

    image load

--- travelRate/urls.py
+++ travelRate/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from city import views as cityViews
 
 from django.conf.urls.static import static
@@ -24,7 +24,6 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', cityViews.home),
-    path('reviews/', include('reviews.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
