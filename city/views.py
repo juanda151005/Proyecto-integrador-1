76c6d681a32002df5cdc1a20142833415d25eaaf
Juanda151005 <juandavidvr151005@gmail.com>
2024-09-03 21:06:21 -0500

    show reviews after clicking on cities

--- city/views.py
+++ city/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse
-
+from reviews.models import Reviews
 from .models import City
 
 # Create your views here.
@@ -14,3 +14,14 @@ def home(request):
         cities=City.objects.all()
     cities = cities.order_by('-rate')
     return render(request, 'home.html', {'searchTerm': searchTerm, 'cities': cities})
+
+def city_reviews(request, city_name):
+    city = get_object_or_404(City, name=city_name)
+    reviews = Reviews.objects.filter(city=city.name)
+
+    context = {
+        'city': city,
+        'reviews': reviews
+    }
+    
+    return render(request, 'city_reviews.html', context)
--- travelRate/urls.py
+++ travelRate/urls.py
@@ -17,14 +17,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from city import views as cityViews
-
+from city.views import city_reviews
 from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', cityViews.home),
-    path('reviews/', include('reviews.urls')),
+    path('reviews/<str:city_name>/', city_reviews, name='city_reviews'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
