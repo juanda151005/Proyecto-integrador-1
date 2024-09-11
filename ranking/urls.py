9dd785d8eff3892aad9632f18eeeadb0d6453f90
Juanda151005 <juandavidvr151005@gmail.com>
2024-09-11 10:13:30 -0500

    show reviews after clicking on a city in ranking

--- ranking/urls.py
+++ ranking/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
+from django.urls import path, include
 from . import views
+from city.views import city_reviews
 
 urlpatterns=[
-    path('',views.top_250_cities,name='ranking')
+    path('',views.top_250_cities,name='ranking'),
+    path('reviews/', include('reviews.urls')),
+    path('reviews/<str:city_name>/', city_reviews, name='city_reviews'),
 ]
